317bb760ed7ae2ded9c1c060320163ec5f9da7a9
Melissa Wheatley <melissa.c.wheatley@gmail.com>
2018-04-05 09:06:26 -0500

    carousel, nav, best sellers

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 "use strict";
 
+let $ = require('jquery');
+
 console.log('main.js has arrived');
+
+$( ".cookie" ).hover(
+    function() {
+      $( this ).addClass( 'd-none' );
+      $( 'img' ).next().removeClass( 'd-none');
+    }, function() {
+      $( this ).removeClass( 'd-none' );
+      $( 'img' ).next().addClass( 'd-none' );
+    }
+  );
